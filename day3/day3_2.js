e1ea5073aa65539f26b32b2d460c3e98b486a41a
Matheus Sansone Pinheiro <matheus.pinheiro_thera@prestador.globo>
2021-12-16 00:39:06 -0300

    day 3 part 2

--- day3/day3_2.js
+++ day3/day3_2.js
@@ -7,4 +7,37 @@ function readFile(file) {
 const content = readFile('./input.txt');
 
 const lines = content.split('\n');
+const lineLenght = lines[0].split('').length;
+
+function Run(customArray, bitLeft, bitRight) {
+    for (let index = 0; index < lineLenght; index++) {
+        let leftBit = 0;
+        let rightBit = 0;
+
+        for (let index2 = 0; index2 < customArray.length; index2++) {
+            const element = customArray[index2].split('')[index];
+
+            if (element == "0")
+                rightBit++;
+            else
+                leftBit++;
+        }
+
+        if (leftBit >= rightBit)
+            customArray = customArray.filter(x => x.split('')[index] == bitLeft)
+        else
+            customArray = customArray.filter(x => x.split('')[index] == bitRight)
+
+        if (customArray.length == 1)
+            break;
+    }
+
+    return customArray;
+}
+
+const oxygen = parseInt(Run([...lines], "1", "0"), 2);
+const co2 = parseInt(Run([...lines], "0", "1"), 2);
+
+console.log(oxygen, co2);
+console.log(oxygen * co2)
 
